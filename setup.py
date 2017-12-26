bc88756de2d81de198a68f512b2b32ac6a793d5d
Aviv <avivga@gmail.com>
2017-12-26 14:01:16 +0200

    add distutils setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name="face-detection",
+	version="1.0",
+	description="dlib face detection wrapper",
+
+	packages=["facedetection"],
+	package_data={'facedetection': ['shape_predictor_68_face_landmarks.dat']},
+
+	requires=["numpy", "dlib"]
+)
